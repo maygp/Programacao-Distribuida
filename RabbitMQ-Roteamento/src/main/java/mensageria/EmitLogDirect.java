e4fc400183d8c21e0aaf7f90acb3e425fb63a088
Mayara <mayaragomper@gmail.com>
2021-11-26 22:35:09 -0300

    Roteamento de Filas

---
+++ RabbitMQ-Roteamento/src/main/java/mensageria/EmitLogDirect.java
@@ -0,0 +1,33 @@
+package mensageria;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+public class EmitLogDirect {
+    private static String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPort(5672);
+
+        String text = "Mayara Gomes Pereira";
+        String tipo = "string";
+
+        try (
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel();
+        ) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+            channel.basicPublish(EXCHANGE_NAME, tipo, null, text.getBytes("UTF-8"));
+            System.out.println(String.format("Send '%s' - %s.", text, tipo));
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
